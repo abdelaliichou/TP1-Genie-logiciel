2be1d1d38f9a44e0a7502f213efe839a3a0a2bfe
Ichou Abdelali <a.ichou@esi-sba.dz>
2024-12-23 15:16:16 +0100

    adding github repo

--- src/main/java/re/forestier/edu/Main.java
+++ src/main/java/re/forestier/edu/Main.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        // ICHOU Abdelali's repository for his project => https://github.com/abdelaliichou/TP1-Genie-logiciel
+
         Player firstPlayer = new Player("Florian", "Ruzberg de Rivehaute", "DWARF", 200, new ArrayList<>(),5);
         UpdatePlayer.addMoney(firstPlayer, 600);
         UpdatePlayer.addXp(firstPlayer, 40);
